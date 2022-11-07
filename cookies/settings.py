042578312ba104f6c42d0baa73eb05fa2e9dc98e
Ikechukwu <ikechukwuandrew4@gmail.com>
2022-11-07 15:29:11 +0100

    editing

--- cookies/settings.py
+++ cookies/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 import django_heroku
 django_heroku.settings(locals())
 
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'cookies.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': os.path.join[BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,11 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
-STATICFILES_DIRS = [BASE_DIR /
-            'static'
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static' )
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 
 # Default primary key field type
